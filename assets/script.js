3f80e9d3a58d84c90d61a79f5c0f8356b9cf1127
Alvin Zulfanur <alvinzulfanur72@gmail.com>
2022-10-11 18:22:06 +0700

    first commit

---
+++ assets/script.js
@@ -0,0 +1,23 @@
+function redirLinkedIn() {
+  window.open('https://www.linkedin.com/in/alvin-zulfanur-bachtiar-2032a7222/');
+}
+function redirGithub() {
+  window.open('https://github.com/AlvinZulfanur');
+}
+function redirTwitter() {
+  window.open('https://twitter.com/tauninisidang');
+}
+function redirInstagram() {
+  window.open('https://www.instagram.com/alzlzxz/');
+}
+
+const UNU = document.getElementById('redirectUNU')
+
+UNU.style.textDecoration = "Underline"
+
+// const paragraph = document.getElementById('Paragraph');
+// let typewriter = new typewriter(paragraph, {
+//     strings: ['Baru Belajar', 'Front End Developer'],
+//     autostart: true,
+//     loop: true
+// });
